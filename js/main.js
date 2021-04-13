d9be4230e14d662ce4f77b6ea77de767bd42ccf5
Laura Caldaronello <caldaronello.l@gmail.com>
2021-04-13 10:25:14 +0200

    Contenuti header e footer con vue

--- js/main.js
+++ js/main.js
@@ -1,7 +1,58 @@
 var app = new Vue({
     el: '#root',
     data: {
-        
+        header: {
+            links: {
+                names: ['home','pages','blog','shop','shortcodes','support','contact'],
+                pathStart: 'img/icons/header/',
+                pathEnd: '.png'
+            }
+        },
+        footer: {
+            map: {
+                nImages: 14,
+                pathStart: 'img/images/map/',
+                pathEnd: '.png'
+            },
+            contacts: {
+                contents: [
+                    {
+                        name: 'postal address',
+                        text: [
+                            'fable care center',
+                            '85 fentiman ave',
+                            'ottawa, ON K1S 0T7'
+                        ]
+                    },
+                    {
+                        name: 'phone & e-mail',
+                        text: [
+                            'phone: 1-800-64-38',
+                            'fax: 1-800-64-39',
+                            'office@fable.com'
+                        ]
+                    },
+                    {
+                        name: 'business hours',
+                        text: [
+                            'monday - friday',
+                            '8.00 am - 5.00 pm',
+                            'weekend closed'
+                        ]
+                    },
+                    {
+                        name: 'sessions',
+                        text: [
+                            'mornings, 8 am - 12 noon',
+                            'afternoons, 1 pm - 5 pm',
+                            'full day, 8 am - 5 pm'
+                        ]
+                    }
+                ],
+                pathStart: 'img/icons/footer/contacts/',
+                pathEnd: '.png'
+            }
+        }
     }
 });
 Vue.config.devtools = true;
